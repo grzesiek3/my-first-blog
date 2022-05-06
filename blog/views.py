25980b282e3852b7a0e1ec173706e6262a8bd1e9
grzesiek3 <gkapusta9@gmail.com>
2022-05-06 11:45:19 +0200

    dodanie widoku i templates dla detali postow i formatowanie CSS

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -6,3 +6,8 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     data = {'posts':posts}
     return render(request, 'post_list.html', data)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    data = {'post':post}
+    return render(request, 'post_detail.html', data)
